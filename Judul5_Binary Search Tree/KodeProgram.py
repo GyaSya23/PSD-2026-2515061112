1773c88c5a7cc696f9a24ec9e3bf6e137b2114e3
GyaSya23 <saputrayoga151@gmail.com>
2026-05-26 21:37:33 +0700

    Update KodeProgram.py

--- Judul5_Binary Search Tree/KodeProgram.py
+++ Judul5_Binary Search Tree/KodeProgram.py
@@ -42,18 +42,15 @@ class BSTNilaiMahasiswa:
             root.right = self.delete_node(root.right, nilai)
 
         else:
-            # Tidak punya anak
             if root.left is None and root.right is None:
                 return None
 
-            # Punya satu anak
             elif root.left is None:
                 return root.right
 
             elif root.right is None:
                 return root.left
 
-            # Punya dua anak
             else:
                 successor = self.find_min_node(root.right)
                 root.key = successor.key
